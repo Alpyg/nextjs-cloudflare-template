7110178bd97842ac2af1882aa5521fdf7c03cc15
Alpyg <alpyg@pm.me>
2024-05-06 18:40:55 -0400

    Fixed Drizzle for D1

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import { drizzle } from "drizzle-orm/d1";
 
-export const db = drizzle(getRequestContext().env.DB);
+export const db = () => drizzle(getRequestContext().env.DB);
